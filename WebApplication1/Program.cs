fcce2abc00e0c9eac497cd2078e7cfb3761fa0bf
Shehan-Amarasinghe <Shehan.nivantha124@gmail.com>
2024-07-21 19:37:52 +0530

    update with POST

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -4,18 +4,20 @@ using WebApplication1;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+const string GetGameEndpointName = "GetGame";
+
 List<GameDto> games = [
     new (
         1,
         "Street Fighter II",
         "Fighting",
-        "cdb",
+        "23.99",
         new DateOnly(1992, 7, 15)),
     new (
         2,
         "Final Fanstasy XIV",
         "Roleplaying",
-        "abc",
+        "18.99",
         new DateOnly(2010, 9, 30))
 ];
 
@@ -23,7 +25,23 @@ List<GameDto> games = [
 app.MapGet("games", () => games);
 
 // GET /games/1
-app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id))
+  .WithName(GetGameEndpointName);
 
-//POST
+//POST /games
+app.MapPost("games", (CreateGameDto newGame) => {
+   GameDto game = new(
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate);
+
+        games.Add(game); 
+
+        return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id}, game);
+});
+
+
+
 app.Run();
